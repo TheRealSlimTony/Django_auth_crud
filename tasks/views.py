d2cfe93c5a4377194e0aa1d6694a76757355e2cf
The.Real.Slim.Tony <antoniomendezrojascr@gmail.com>
2023-03-23 20:28:02 -0600

    fix async issue of render

--- tasks/views.py
+++ tasks/views.py
@@ -1,23 +1,26 @@
-from django.shortcuts import render, redirect, get_object_or_404
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-from django.contrib.auth.models import User
-from django.contrib.auth import login, logout, authenticate
-from django.db import IntegrityError
-from .forms import TaskForm
-from .models import Task
-from django.utils import timezone
-from django.contrib.auth.decorators import login_required
+import asyncio
+
+import httpx
 # Create your views here.
 import requests as ri
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth.models import User
+from django.db import IntegrityError
+from django.shortcuts import get_object_or_404, redirect, render
+from django.utils import timezone
+from asgiref.sync import sync_to_async
 
-import asyncio
-import httpx
+from .forms import TaskForm
+from .models import Task
+
 
 async def fetch_facts(api_key, limit):
     headers = {'X-Api-Key': api_key}
     url = f'https://api.api-ninjas.com/v1/facts?limit={limit}'
     async with httpx.AsyncClient() as client:
-        response = await client.get(url, headers=headers, timeout=10.0)
+        response = await client.get(url, headers=headers, timeout=100.0)
     if response.status_code == httpx.codes.ok:
         fact = response.json()[0]['fact']
     else:
@@ -27,13 +30,18 @@ async def fetch_facts(api_key, limit):
 async def home(request):
     api_key = 'v6thHnV+uQHj1bIVxSvv3w==km3pAo6anq2N1iPi'
     limit = 1
+    fact = ""
     fact = await fetch_facts(api_key, limit)
     print(request.method)
 
-    if fact == "Error fetching fact":
+    if fact == "Error fetching fact" or fact == "":
         fact = "Por favor saquenme de latam"
+    
+    # wrap the render function with sync_to_async
+    render_func = sync_to_async(render)
+    rendered = await render_func(request, 'home.html', {'fact': fact})
+    return rendered
 
-    return render(request, 'home.html', {'fact': fact})
 def signup(request):
     if request.method == 'GET':
         return render(request, 'signup.html', {
@@ -85,12 +93,10 @@ def signin(request):
         })
     else:
         if request.method == 'POST':
-
             user = request.POST['username']
             password = request.POST['password']
             # print(user, password)
             user = authenticate(request, username=user, password=password)
-
             if user is None:
                 return render(request, 'signin.html', {
                     'form': AuthenticationForm,
@@ -98,6 +104,8 @@ def signin(request):
                 })
             else:
                 login(request, user)
+                print(user)
+                print(type(user))
                 return redirect('home')
 
 @login_required
